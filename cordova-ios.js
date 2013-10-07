70d593553a3ba36143f05b676a3248f1c1c6641e
Pauli B <pauli.b@outlook.com>
2013-10-07 11:54:59 +0100

    eventlisterner for spash

--- cordova-ios.js
+++ cordova-ios.js
@@ -17,9 +17,9 @@ Session.set('ios-webview', 'in-webview-ios');
  to you under the Apache License, Version 2.0 (the
  "License"); you may not use this file except in compliance
  with the License.  You may obtain a copy of the License at
- 
+
      http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing,
  software distributed under the License is distributed on an
  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -1666,7 +1666,7 @@ module.exports = [
         ]
     }
 ]
-}); 
+});
 
 //
 //      Device
@@ -1848,3 +1848,10 @@ module.exports = function(strUrl, strWindowName, strWindowFeatures) {
 };
 
 });
+
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+    function onDeviceReady() {
+        navigator.splashscreen.hide();
+    }
