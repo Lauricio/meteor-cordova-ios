de654abc4da6dc9871ae7e4aa035289795ba9382
Lauricio Su <me@lauricio.com>
2013-10-08 11:17:40 +0300

    moved status bar on device ready

--- control.js
+++ control.js
@@ -1,12 +1,13 @@
 
-// document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("deviceready", onDeviceReady, false);
 
-//     function onDeviceReady() {
-//         navigator.splashscreen.hide();
-//     }
+    function onDeviceReady() {
+        // navigator.splashscreen.hide();
+        iosStatusBar = window.plugins.statusBar;
+    }
 
 // for iOS specific logic and styles, class name: 'in-webview-ios'
 Session.set('ios-webview', 'in-webview-ios');
 
-iosStatusBar = window.plugins.statusBar;
+
 
