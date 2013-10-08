531541caa4ffe4190b3d728f60a89f20beb83ac2
Lauricio Su <me@lauricio.com>
2013-10-08 13:55:38 +0300

    comment fix

--- cordova-ios.js
+++ cordova-ios.js
@@ -1590,7 +1590,8 @@ module.exports = [
 //      Device
 //
 
-cordova.define("org.apache.cordova.device.device", function(require, exports, module) {/*
+cordova.define("org.apache.cordova.device.device", function(require, exports, module) {
+/*
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
